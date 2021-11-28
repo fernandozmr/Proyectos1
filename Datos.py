4c1ae0a17235d63727f2ab0e06032da3021fef70
fernandozmr <94845179+fernandozmr@users.noreply.github.com>
2021-11-28 19:47:41 -0300

    Update Datos.py

--- Datos.py
+++ Datos.py
@@ -6,5 +6,5 @@ age = input("age: \n")
 adress = input ("Localidad: \n").upper()
 pais = input ("Pais: \n").upper()
 
-print("\nLos datos ingresados han sido: \n")
+print("\nLos datos que usted ha ingresado fueron: \n")
 print(f"name: {nombre}, \nsurname: {apellido} \nage: {age} años. \nDe: {adress} \nPais {pais} \n¡¡Bienvenido!!")
