103120f94efe5c8b8aa320a3db7f7b8fdd83e1b0
fernandozmr <fernandozmr21@gmail.com>
2021-11-28 19:20:17 -0300

    Primer commit

---
+++ Datos.py
@@ -0,0 +1,8 @@
+print ("Ingrese sus datos")
+
+nombre = input("name: \n").upper()
+apellido = input ("surname: \n").lower()
+age = input("age: \n")
+
+print("\nLos datos ingresados han sido: \n")
+print(f"name: {nombre}, \nsurname: {apellido} y \nage: {age} años. \n¡¡Bienvenido!!")
